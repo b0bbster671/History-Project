4acdd8517447dbbeefc51b0b4e759b531f075ef2
Maxim Ivanov <b0bbster671@gmail.com>
2023-11-26 21:12:12 +0500

    Сделал первую страницу

---
+++ script.js
@@ -0,0 +1,10 @@
+var firstStep = document.getElementById('firstStep');
+var header = document.getElementById('header');
+var pugalo = document.getElementById('pugalo');
+
+function beginClicked()
+{
+    firstStep.style.display= 'none';
+    header.style.display= 'none';
+    pugalo.style.display= 'flex';
+}
