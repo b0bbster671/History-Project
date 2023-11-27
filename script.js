fe9a89ee6b4b54b50c611277ac7a2ab946d100fd
Maxim Ivanov <b0bbster671@gmail.com>
2023-11-27 09:46:04 +0500

    Добавил Лого+ разделение кнопок

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var firstStep = document.getElementById('firstStep');
 var header = document.getElementById('header');
 var pugalo = document.getElementById('pugalo');
+var mainImg = document.getElementById('mainImg');
 
 function beginClicked()
 {
     firstStep.style.display= 'none';
     header.style.display= 'none';
     pugalo.style.display= 'flex';
+    mainImg.style.filter = 'grayscale(50%)'
 }
